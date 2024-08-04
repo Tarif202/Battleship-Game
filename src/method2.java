3acb8987b71bbc923d86da06751f64fa7ac4b80b
Tarif202 <150162006+Tarif202@users.noreply.github.com>
2024-08-04 17:53:00 +0100

    Battleship-Game

---
+++ src/method2.java
@@ -0,0 +1,62 @@
+
+public class method2 {
+
+    static char[][] board = {
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'S', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', 'S', 'S', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', 'S', 'S', 'S'}
+    };
+    public static int Q2_ValidBoard(char board[][]) {
+    	boolean sizeallgoodmyman = true;
+        if (board == null) {
+            sizeallgoodmyman = false;
+
+            return -1; 
+        }
+        
+        if (board.length != 10) {
+            sizeallgoodmyman = false;
+
+            return -2; 
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            if (board[i].length != 10) {
+	            sizeallgoodmyman = false;  
+
+                return -2; 
+            }
+        }
+             
+        boolean validSquare = false;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                char box = board[i][j];
+                if (!(box == 'S' || box == '*' || box == '.') && (sizeallgoodmyman == true)) {
+                    return -3; 
+                } else {
+                    validSquare = true;
+                }
+            }
+        }
+
+        if (validSquare==true) {
+            return 1; //  board is valid
+        } 
+        	return 91; //default value
+    }
+
+
+    
+	public static void main(String[] args) {
+		System.out.println(Q2_ValidBoard(board));
+	}
+
+}
---
+++ src/method3.java
@@ -0,0 +1,68 @@
+import java.util.StringTokenizer;
+
+public class method3 {
+	
+
+	 static char[][] board = {
+			 	{'.', 'S', 'S', 'S', '.', '.', '.', '.', '.', '.'},
+			    {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+			    {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+			    {'*', '*', '.', 'S', '*', '.', '.', '.', '.', '.'},
+			    {'.', '.', '.', '.', '.', '*', '*', '*', '*', '*'},
+			    {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+			    {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+			    {'.', '.', '.', '*', '*', '*', '*', '.', '.', '.'},
+			    {'.', '.', '.', '.', '.', '.', '*', '*', '*', '*'},
+			    {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}
+			};
+	   
+	 public static int Q3_NumberSunk(char board[][]) {
+        int count = 0;
+        int count2 = -1;
+       
+        int something=0;
+     
+        String token2;
+        String token;
+        for (int i = 0; i < 10; i++) {
+            String row = new String(board[i]);
+            StringTokenizer tokenizer = new StringTokenizer(row, ".");
+            while(tokenizer.hasMoreTokens()) {
+                 token = tokenizer.nextToken(); 
+                if (!token.isEmpty() && !token.contains("S") && !(token.length()==1) && token.contains("*") )  {         	
+                    count++;
+                }
+           
+               
+            }}
+
+        for (int i = 0; i < 10; i++) {
+            String row = new String(board[i]);
+            StringTokenizer tokenizer7 = new StringTokenizer(row);
+            while(tokenizer7.hasMoreTokens()) {
+                 token2 = tokenizer7.nextToken();    
+                
+               if (!token2.isEmpty() &&  !(token2.contains("S")) && !(token2.contains("*")) && token2.contains("."))  {         	
+            	 
+					something++;
+                }
+           
+               
+            }}
+   
+        if(something == 10 ) {  
+        	return count2;
+        }
+        		
+       
+        return count;
+        
+    }
+       
+	public static void main(String[] args) {
+		System.out.println(Q3_NumberSunk(board));
+	}
+	
+
+
+}
