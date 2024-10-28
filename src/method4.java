6d9728581bfae8dd50d9c40ccac549bbc379a47d
Tarif202 <150162006+Tarif202@users.noreply.github.com>
2024-10-28 19:12:06 +0000

    Update method4.java
    proper indentation

--- src/method4.java
+++ src/method4.java
@@ -1,61 +1,50 @@
-
 public class method4 {
-	
-	
-	   static char[][] board = {
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-	         
-	    };
-	
-	   public static int Q4_Hit(char board[][],int row,char col) {
-	    	 int invalidColumn = -2;
-	    	int invalidRow = -1;
-	    	int rowIndex = row - 1;
-	    	int columnIndex = col -'A';
-	    	 int hit=1;
-	         int miss=2;
-	         int repeatedhit=3;
-	    	  if (rowIndex < 0 || rowIndex > 9) {
-	              // Check if column is also invalid
-	             
-	              return invalidRow;
-	             
-	          }
-	    	  if (col < 'A' || col > 'J') {
-	              return invalidColumn; // Only column is invalid
-	          }
-	    	char test = board[rowIndex][columnIndex]; 
-	        if (test=='.') {
-	        
-	       	return miss;
-	        }
-	        
-	        if (test=='S') {
-	           	return hit;
-	            }
-	        
-	        if (test=='*') {
-	           	return repeatedhit;
-	            }
-	        
-	        
 
-	        return 7; // Both row and column are valid
-	    }
+    static char[][] board = {
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+    };
 
-	   
-	  
+    public static int Q4_Hit(char board[][], int row, char col) {
+        int invalidColumn = -2;
+        int invalidRow = -1;
+        int rowIndex = row - 1;
+        int columnIndex = col - 'A';
+        int hit = 1;
+        int miss = 2;
+        int repeatedHit = 3;
 
-	
-	public static void main(String[] args) {
-		System.out.println(Q4_Hit(board , 0, 'A'));
-	}
+        if (rowIndex < 0 || rowIndex > 9) {
+            return invalidRow;
+        }
+        if (col < 'A' || col > 'J') {
+            return invalidColumn;
+        }
+
+        char test = board[rowIndex][columnIndex];
+        if (test == '.') {
+            return miss;
+        }
+
+        if (test == 'S') {
+            return hit;
+        }
+
+        if (test == '*') {
+            return repeatedHit;
+        }
+
+        return 7; // Both row and column are valid
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Q4_Hit(board, 0, 'A'));
+    }
 }
