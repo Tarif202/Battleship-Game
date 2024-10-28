bf01e64efe917019cae3cb6815e680ab79a33e89
Tarif202 <150162006+Tarif202@users.noreply.github.com>
2024-10-28 19:09:51 +0000

    Update method2.java
    proper indentation

--- src/method2.java
+++ src/method2.java
@@ -1,4 +1,3 @@
-
 public class method2 {
 
     static char[][] board = {
@@ -13,50 +12,46 @@ public class method2 {
             {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
             {'.', '.', '.', '.', '.', '.', '.', 'S', 'S', 'S'}
     };
+
     public static int Q2_ValidBoard(char board[][]) {
-    	boolean sizeallgoodmyman = true;
+        boolean sizeallgoodmyman = true;
         if (board == null) {
             sizeallgoodmyman = false;
-
-            return -1; 
+            return -1;
         }
-        
+
         if (board.length != 10) {
             sizeallgoodmyman = false;
-
-            return -2; 
+            return -2;
         }
-        
+
         for (int i = 0; i < 10; i++) {
             if (board[i].length != 10) {
-	            sizeallgoodmyman = false;  
-
-                return -2; 
+                sizeallgoodmyman = false;
+                return -2;
             }
         }
-             
+
         boolean validSquare = false;
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
                 char box = board[i][j];
                 if (!(box == 'S' || box == '*' || box == '.') && (sizeallgoodmyman == true)) {
-                    return -3; 
+                    return -3;
                 } else {
                     validSquare = true;
                 }
             }
         }
 
-        if (validSquare==true) {
-            return 1; //  board is valid
-        } 
-        	return 91; //default value
+        if (validSquare == true) {
+            return 1; // board is valid
+        }
+        return 91; // default value
     }
 
-
-    
-	public static void main(String[] args) {
-		System.out.println(Q2_ValidBoard(board));
-	}
+    public static void main(String[] args) {
+        System.out.println(Q2_ValidBoard(board));
+    }
 
 }
